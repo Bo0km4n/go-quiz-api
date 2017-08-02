c66791339ae3acc65b03721670dc05657100466a
川部勝也 <al14031@shibaura-it.ac.jp>
2017-08-03 03:12:12 +0900

    mongo

--- api/quiz/v1/quiz.go
+++ api/quiz/v1/quiz.go
@@ -1,6 +1,15 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"math/rand"
+	"strconv"
+
+	"github.com/KatsuyaKawabe/go-quiz-api/models"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
 func Create(ctx *gin.Context) {
 
@@ -8,7 +17,33 @@ func Create(ctx *gin.Context) {
 	return
 }
 
+// GET /api/v1/quiz?difficulty=1,2,3
 func Index(ctx *gin.Context) {
-	ctx.JSON(200, gin.H{"message": "quiz Index"})
+	qS := ctx.Query("difficulty")
+	parseQS, _ := strconv.ParseUint(qS, 10, 32)
+	difficulty := uint(parseQS)
+
+	var qs []models.Quiz
+	sess, _ := mgo.Dial(viper.GetString("mongo.host"))
+	defer sess.Close()
+	db := sess.DB(viper.GetString("mongo.db"))
+	col := db.C("quiz")
+
+	//  難易度指定でいくつかのクイズを取得
+	colCount, err := col.Find(bson.M{"difficulty": difficulty}).Count()
+	if err != nil {
+		ctx.JSON(400, "Invalid request")
+		return
+	}
+
+	if colCount == 0 {
+		ctx.JSON(404, "Quiz Not found")
+		return
+	}
+
+	query := col.Find(bson.M{"difficulty": difficulty})
+	query.All(&qs)
+	quiz := qs[rand.Intn(colCount)]
+	ctx.JSON(200, quiz)
 	return
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/KatsuyaKawabe/go-quiz-api/db"
 	"github.com/KatsuyaKawabe/go-quiz-api/router"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -12,7 +11,7 @@ import (
 func init() {
 	viper.AddConfigPath("./config")
 	viper.SetConfigType("yaml")
-
+	viper.SetConfigName("dev")
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println("config error", err)
 	}
@@ -26,7 +25,6 @@ func ListenAPI() {
 	api.HandleMethodNotAllowed = true
 
 	router.V1(api)
-	db.SetupDB()
 	api.Run(":" + viper.GetString("api.port"))
 }
 
